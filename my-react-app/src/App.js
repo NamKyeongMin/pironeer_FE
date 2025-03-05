7d65293d43ee6efb14b8cb0da89ef5a0f78351fe
NamKyeongMin <kyeongmin35@gachon.ac.kr>
2025-03-05 14:50:43 +0900

    useEffect로 선택적 렌더링 구현하기

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,16 +1,36 @@
 import Button from "./Button";
 import styles from "./App.module.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
-  console.log("call an api");
+  const onChange = (event) => setKeyword(event.target.value);
+  const iRunOnlyOnce = () => {
+    if (keyword !== "" && keyword.length > 5) {
+      console.log("SEARCH FOR", keyword);
+    }
+  };
+  useEffect(iRunOnlyOnce, [keyword]);
+  useEffect(() => {
+    console.log("I run when 'counter' changes.");
+  }, [counter]);
+  useEffect(() => {
+    console.log("I run when keyboard & counter change.");
+  }, [keyword, counter]);
   return (
     <div className="App">
       <h1 className={styles.title}>Welcome back!!!</h1>
       <Button text="Continue" />
+
       <h1>{counter}</h1>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
       <button onClick={onClick}>click me</button>
     </div>
   );
