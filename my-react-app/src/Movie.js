b16665e0536574f087d30ad27b2b04ce6ada974c
NamKyeongMin <kyeongmin35@gachon.ac.kr>
2025-03-05 23:41:47 +0900

    자료형이 배열인 state의 modify 사용하기

---
+++ my-react-app/src/Movie.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+function App() {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  const onChange = (event) => {
+    setTodo(event.target.value);
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (todo === "") {
+      return; // function stop!
+    }
+    setTodos((currentArray) => [todo, ...currentArray]);
+    setTodo("");
+  };
+  return (
+    <div>
+      <h1>My To-Do List ({todos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={todo}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
