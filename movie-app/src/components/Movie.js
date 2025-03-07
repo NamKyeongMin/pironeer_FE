1d642e38e878fa4527112b0bc9294dc0844af529
NamKyeongMin <kyeongmin35@gachon.ac.kr>
2025-03-07 22:15:23 +0900

    movie-app 최종 css까지 추가하기

--- movie-app/src/components/Movie.js
+++ movie-app/src/components/Movie.js
@@ -1,19 +1,27 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom"; // a 태그 대신 사용하여, (불필요한) 전체 페이지의 리로딩을 막음
+import styles from "../styles/Home.module.css";
 
 function Movie({ id, movieImage, title, summary, genres }) {
+  const message = summary ? summary : "There is no summary yet. 🥲";
   return (
-    <div>
-      <img src={movieImage} alt={title} />
-      <h2>
-        <Link to={`/movie/${id}`}>{title}</Link>
-      </h2>
-      <p>{summary ? summary : "There is no summary! 🥲"}</p>
-      <ul>
-        {genres.map((genre) => (
-          <li key={genre}>{genre}</li>
-        ))}
-      </ul>
+    <div className={styles.movieContainer}>
+      <img className={styles.poster} src={movieImage} alt={title} />
+      <div>
+        <h2 className={styles.title}>
+          <Link to={`/movie/${id}`}>{title}</Link>
+        </h2>
+        <p className={styles.contents}>
+          <span className={styles.infoOptions}>Summary:</span>{" "}
+          {message.length > 235 ? `${message.slice(0, 235)}...` : message}
+        </p>
+        <p className={styles.infoOptions}>Genres:</p>
+        <ul className={styles.contents}>
+          {genres.map((genre) => (
+            <li key={genre}>{genre}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
@@ -22,7 +30,7 @@ Movie.propTypes = {
   id: PropTypes.number.isRequired,
   movieImg: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
-  summary: PropTypes.string,
+  summary: PropTypes.string.isRequired,
   genres: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 export default Movie;
