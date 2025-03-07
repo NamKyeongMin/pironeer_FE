b81cd33c97525686be7b0df797f4e5c79adeabd8
NamKyeongMin <kyeongmin35@gachon.ac.kr>
2025-03-07 17:22:20 +0900

    async 함수와 fetch API를 이용해 id로 영화 식별, detail page 최종 구현하기(챌린지)

--- movie-app/src/routes/Detail.js
+++ movie-app/src/routes/Detail.js
@@ -1,5 +1,44 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import Movie from "../components/Movie";
+
 function Detail() {
-  return <h1>Detail</h1>;
+  const [loading, setLoading] = useState(true);
+  const [movieToFind, SetMovieToFind] = useState("");
+  const { id } = useParams();
+  async function getMovie() {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    console.log(json);
+    SetMovieToFind(json.data.movie);
+    setLoading(false);
+  }
+  useEffect(() => {
+    getMovie();
+    console.log(movieToFind);
+  }, []);
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading Movie Info...</h1>
+      ) : (
+        <div>
+          <h1>🎬 Movie Info 🍿</h1>
+          <div>
+            <Movie
+              key={movieToFind.id}
+              id={movieToFind.id}
+              movieImage={movieToFind.large_cover_image}
+              title={movieToFind.title}
+              summary={movieToFind.summary}
+              genres={movieToFind.genres}
+            />
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Detail;
